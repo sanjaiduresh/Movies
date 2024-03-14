8433055341aaf97d7d043cdd49b82460b891794c
sanjaiduresh <sanjaidureshowo@gmail.com>
2024-03-14 11:39:24 +0530

    movie fetch

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function MovieDetail() {
+    const{id}=useParams();
+    console.log(id);
+    const [movie,setMovie]=useState([]);
+
+    useEffect(()=>{
+        fetch(`https://65f16b99034bdbecc762724b.mockapi.io/movie/${id}`,{
+            method:"GET"
+        }).then((data)=>data.json()).then((res)=>setMovie(res));
+    },[]);
+    console.log(movie);
   return (
     <div>MovieDetail</div>
   )
