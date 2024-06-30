85a390708272f36dbce1ce2c863dcf2e5fb068ad
sanjaiduresh <sanjaidureshowo@gmail.com>
2024-06-30 19:24:02 +0530

    update

--- src/Topbar.js
+++ src/Topbar.js
@@ -7,23 +7,23 @@ import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import { Logout } from './Logout';
 
-export default function Topbar({mode,setMode}) {
-    const navigate=useNavigate();
+export default function Topbar({ mode, setMode }) {
+  const navigate = useNavigate();
   return (
-   
-      <AppBar position="static">
-        <Toolbar>
-          <Button variant="inherit" onClick={()=>navigate("/portal/home")}>
-            Home
-            </Button>
-          <Button variant="inherit" onClick={()=>navigate("/portal/movielist")}>Movies</Button>
-          <Button variant="inherit" onClick={()=>navigate("/portal/addmovie")}>AddMovie</Button>
-          <Button style={{marginLeft:"60%"}} startIcon={mode==="light"?<Brightness4Icon />:<Brightness7Icon />} color='inherit'
-          onClick={()=>setMode(mode==="light" ? "dark" : "light")}>
-            {mode==="light"? "Dark":"Light"}Mode
-          </Button>
-          <Button color="inherit" onClick={()=>Logout()}>Logout</Button>
-        </Toolbar>
-      </AppBar>
+
+    <AppBar position="static">
+      <Toolbar>
+        <Button variant="inherit" onClick={() => navigate("/portal/home")}>
+          Home
+        </Button>
+        <Button variant="inherit" onClick={() => navigate("/portal/movielist")}>Movies</Button>
+        <Button variant="inherit" onClick={() => navigate("/portal/addmovie")}>AddMovie</Button>
+        <Button style={{ marginLeft: "60%" }} startIcon={mode === "light" ? <Brightness4Icon /> : <Brightness7Icon />} color='inherit'
+          onClick={() => setMode(mode === "light" ? "dark" : "light")}>
+          {mode === "light" ? "Dark" : "Light"}Mode
+        </Button>
+        <Button color="inherit" onClick={() => Logout()}>Logout</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
